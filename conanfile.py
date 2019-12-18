e726cfe1dc5ae4b7c35583cb2dbb5eed6345f32a
Craig Sturdy <sturd@protonmail.ch>
2019-12-18 13:13:30 +0000

    Initial conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake, tools
+
+
+class BinacppConan(ConanFile):
+    name = "binacpp"
+    settings = "os", "compiler", "build_type", "arch"
+    description = "Binance C++ library "
+    url = "https://github.com/tensaix2j/binacpp"
+    license = "MIT"
+    author = "https://github.com/tensaix2j"
+    generators = "cmake"
+    requires = [
+        "jsoncpp/1.8.4@theirix/stable",
+        "libcurl/7.56.1@bincrafters/stable",
+        "libwebsockets/2.4.0@bincrafters/stable"
+    ]
+
+    def source(self):
+        git = tools.Git()
+        git.clone("https://github.com/sturd/binacpp.git")
+        git.checkout("cmake")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.verbose = True
+
+        cmake.configure(build_folder="cmake")
+        cmake.build()
+
+    def package(self):
+        self.copy("*")
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
